bfbde908bf5cfead9a0e98bab84e1ecb00eb3959
Ted Won <tkwon09@gmail.com>
2015-06-18 22:50:16 -0700

    Finished first implementation of the Grammar class, tested in scene TestProceduralGeneration using script TestGrammar.

--- Assets/ProceduralGeneration/Grammar.cs
+++ Assets/ProceduralGeneration/Grammar.cs
@@ -11,32 +11,37 @@
  * Rules should be formatted as:
  * 	<symbol><generation1>:<generation2>:<generation3> ...
  * 
- * To avoid non-terminating generations, Grammar requires a max string length parameter.
+ * To avoid non-terminating generations, Grammar requires a max iteration parameter.
  */
+
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grammar {
-
-	private HashSet<char> terminals;			// Terminals. Only valid as right-hand-sides
-	private HashSet<char> symbols;				// Non-terminals. These are valid left-hand-sides
-	private Dictionary<char, HashSet<char[]>> rules;		// Rules. righthandsides generate lefthandsides.
-
+	
+	private HashSet<char> terminals;					// Terminals. Only valid as right-hand-sides
+	private HashSet<char> symbols;						// Non-terminals. These are valid left-hand-sides
+	private Dictionary<char, HashSet<char[]>> rules;	// Rules. righthandsides generate lefthandsides.
+	private char seed;
+	
 	public Grammar()
 	{
 		symbols = new HashSet<char>();
 		terminals = new HashSet<char>();
 		rules = new Dictionary<char, HashSet<char[]>>();
+		seed = '\0';
 	}
 
-	public bool loadFromFile(string filepath)
+	/* Loads a grammar from a text file resource.
+	 */
+	public bool LoadFromFile(string filepath)
 	{
 		// Get grammar file as a TextAsset
 		TextAsset data = (TextAsset) Resources.Load (filepath, typeof(TextAsset));
-
+		
 		// Attempt to load the grammar from the file's contents
-		if (!loadFromString (data.text))
+		if (!LoadFromString (data.text))
 		{
 			Debug.Log ("Grammar file " + filepath + "failed to load!");
 			return false;
@@ -44,12 +49,15 @@ public class Grammar {
 		return true;
 	}
 
-	public bool loadFromString(string grammarstring)
+	/* Loads a grammar from a string. Expected format is described in detail at the top of the file.
+	 * WARNING: this function may partially load a grammar before failing.
+	 */
+	public bool LoadFromString(string grammarstring)
 	{
 		terminals.Clear ();
 		symbols.Clear ();
 		rules.Clear ();
-
+		
 		// Attempt to read the string
 		StringReader reader = new StringReader(grammarstring);
 		
@@ -108,7 +116,7 @@ public class Grammar {
 			}
 			symbols.Add(line[i]);
 		}
-
+		
 		// now extract the rules
 		line = reader.ReadLine();
 		int count = 1;
@@ -120,7 +128,7 @@ public class Grammar {
 				Debug.Log ("Error loading grammar: rule " + count + " is missing lhs!");
 				return false;
 			}
-            if (!symbols.Contains(lhs))
+			if (!symbols.Contains(lhs))
 			{
 				if (terminals.Contains(lhs))
 				{
@@ -132,56 +140,145 @@ public class Grammar {
 				}
 				return false;
 			}
+			
+			// if the symbol doesn't have any rules yet, add a new rule. Otherwise, add to the existing rule.
 			HashSet<char[]> rhs = new HashSet<char[]>();
-
-			// remove the generating symbol from the beginning of the rule
-			line = line.Substring(1);
-
-			// parse each generation separately
-			string[] rhsArr = line.Split(':');
-			for (int i = 0; i < rhsArr.Length; i++)
-			{
-				for (int j = 0; j < rhsArr[i].Length; j++)
-				{
-					if (!symbols.Contains(rhsArr[i].ToCharArray()[j]) && !terminals.Contains (rhsArr[i].ToCharArray()[j]))
-					{
-						Debug.Log ("Error loading grammar: generation " + rhsArr[i] + " in rule " + count + " has an element " + rhsArr[i][j] + " that is not a symbol or terminal!");
-                    	return false;
-					}
-					if (rhs.Contains(rhsArr[i].ToCharArray()) || (rules.ContainsKey(lhs) && rules[lhs].Contains(rhsArr[i].ToCharArray())))
-					{
-						Debug.Log ("Error loading grammar: generation " + rhsArr[i] + " in rule " + count + " is a duplicate of another generation!");
-						return false;
-                    }
-					rhs.Add(rhsArr[i].ToCharArray());
-				}
-            }
-
-			// add the complete rule to the grammar
-			if (rules.ContainsKey(lhs))
-			{
-				foreach (char[] element in rhs)
-				{
-					rules[lhs].Add(element);
-				}
-			}
-			else
+			if (!rules.ContainsKey(lhs))
 			{
 				rules.Add(lhs, rhs);
 			}
-            
+			else
+			{
+				rhs = rules[lhs];
+			}
+
+			// if the seed is the null character, use the first rule's generating symbol as the seed
+			if (seed == '\0')
+			{
+				seed = line[0];
+			}
+
+			// remove the generating symbol from the beginning of the string
+			line = line.Substring(1);
+			
+			// parse each generation separately. generations must be split by ':'
+			string[] rhsArr = line.Split(':');
+			for (int i = 0; i < rhsArr.Length; i++)
+			{
+				// convert into character array
+				char[] genCharArr = rhsArr[i].ToCharArray();
+
+				// make sure that the generations only use valid terminals and symbols
+				for (int j = 0; j < rhsArr[i].Length; j++)
+				{
+					if (!symbols.Contains(genCharArr[j]) && !terminals.Contains (genCharArr[j]))
+					{
+						Debug.Log ("Error loading grammar: generation " + rhsArr[i] + " in rule " + count + " has an element " + rhsArr[i][j] + " that is not a symbol or terminal!");
+						return false;
+					}
+				}
+
+				// keep each generation from the same generating symbol unique
+				if (!rhs.Contains(genCharArr))
+				{
+					rhs.Add(genCharArr);
+				}
+			}
+			
 			line = reader.ReadLine ();
 			count++;
 		}
 		return true;
 	}
 
-	public string generate(int maxLength)
+	/* Clears loaded terminals, symbols and rules.
+	 */
+	public void Clear()
 	{
-		return null;
+		symbols.Clear();
+		terminals.Clear();
+		rules.Clear();
+		seed = '\0';
 	}
 
-	public string getInfo()
+	/* Call recursive generation function on the seed symbol. User must provide a max depth and final terminating
+	 * character to ensure the function halts.
+	 */
+	public string GenerateWord(int maxIterations, char finalTerminal)
+	{
+		if (seed == '\0')
+		{
+			Debug.Log("Grammar has no seed!");
+			return null;
+		}
+
+		// return the result of recursive generation
+		return GenerateRec (seed, 1, maxIterations, finalTerminal);
+	}
+
+	/* The main recursive generating algorithm. We have ensured that the recursive generation halts by forcing the
+	 * user to specify a max number of iterations and a final terminating symbol.
+	 */
+	private string GenerateRec(char gen, int currIteration, int maxIterations, char finalTerminal)
+	{
+		List<char[]> generations = new List<char[]>();
+
+		// if we are at maximum allowed string length, remove all non-terminals from possible generations
+		if (currIteration == maxIterations)
+		{
+			foreach(char[] generation in rules[gen])
+			{
+				// check if there is a non-terminal in the current generation
+				bool isTerminating = true;
+				for (int i = 0; i < generation.Length; i++)
+				{
+					if (!terminals.Contains(generation[i]))
+					{
+						isTerminating = false;
+						break;
+					}
+				}
+				if (isTerminating)
+				{
+					generations.Add(generation);
+				}
+			}
+
+			// if there are no terminating generations, return the finalTerminal
+			if (generations.Count == 0)
+			{
+				return finalTerminal.ToString();
+			}
+		}
+		else 
+		{
+			generations.AddRange(rules[gen]);
+		}
+
+		// select a random generation
+		int index = Random.Range(0, generations.Count);
+
+		// parse the new generation and see if we need to resolve any non-terminals
+		string ret = "";
+		foreach(char e in generations[index])
+		{
+			if (symbols.Contains(e))
+			{
+				ret += GenerateRec (e, currIteration + 1, maxIterations, finalTerminal);
+			}
+			else
+			{
+				ret += e;
+			}
+		}
+
+		// finally, return the resulting string of terminals
+		return ret;
+	}
+
+	/* Returns a string containing information about the grammar
+	 */
+	public string GetInfo()
 	{
 		string info = "";
 		info += terminals.Count + "\tterminals\n\t";
@@ -189,21 +286,21 @@ public class Grammar {
 		{
 			info += terminal + ", ";
 		}
-		info.Remove(info.Length - 3);
+		info = info.Remove(info.Length - 2);
 		info += "\n";
-
+		
 		info += symbols.Count + "\tsymbols\n\t";
 		foreach (char symbol in symbols)
 		{
 			info += symbol + ", ";
 		}
-		info.Remove(info.Length - 3);
+		info = info.Remove(info.Length - 2);
 		info += "\n";
-
+		
 		info += rules.Count + "\trules\n";
 		foreach (char key in rules.Keys)
 		{
-			info += "\t" + key + ": ";
+			info += "\t" + key + " (" + rules[key].Count + "): ";
 			foreach (char[] gen in rules[key])
 			{
 				for (int i = 0; i < gen.Length; i++)
@@ -212,10 +309,10 @@ public class Grammar {
 				}
 				info += ", ";
 			}
-			info.Remove(info.Length - 2);
+			info = info.Remove(info.Length - 2);
 			info += "\n";
 		}
-
+		
 		return info;
 	}
 }
--- Assets/ProceduralGeneration/TestGrammar.cs
+++ Assets/ProceduralGeneration/TestGrammar.cs
@@ -2,12 +2,21 @@
 using System.Collections;
 
 public class TestGrammar : MonoBehaviour {
-
+	
 	// Use this for initialization
-	void Start () {
-		string grammar = "abc\nBC\nBCab\nCc:a\nBb";
-		Grammar test = new Grammar();
-		test.loadFromString(grammar);
-		Debug.Log(test.getInfo());
+	void Start ()
+	{
+		string grammarStr = "abc\nBC\nBCaC\nCc:B\nBbBC";
+
+		Grammar testGrammar = new Grammar();
+
+		testGrammar.LoadFromString(grammarStr);
+
+		Debug.Log(testGrammar.GetInfo());
+
+		for (int i = 0; i < 100; i++)
+		{
+			Debug.Log(testGrammar.GenerateWord(5, 'F'));
+		}
 	}
 }
